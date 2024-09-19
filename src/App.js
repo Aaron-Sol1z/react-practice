b3190be6ca4c36b0d9adc1e02fbdca2b058d509c
Aaron Soliz <aaronmsoliz@hotmail.com>
2024-09-19 15:17:24 -0700

    Added files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Body from './Body.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is the top of the page</h1>
+      <Body />
+    </div>
+  );
+}
+
+export default App;
